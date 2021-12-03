d3b4dc00fb5ff68eb4973c4518723765c0022e5f
Samuel Morales <sdmorales@uninorte.edu.co>
2021-12-03 17:24:16 -0500

    edicion de links

--- src/Dashboardexterno.js
+++ src/Dashboardexterno.js
@@ -15,7 +15,7 @@ function Dashboardexterno(){
                 href3={"#"} icon3={"fas fa-handshake"} card3={"Solicitar Convenios de Pagos"}
                 card3_1={"Convenios de Pagos"} href1card3={"#"} name1card3={"Convenios de Pagos"}
                 titulo2={"Pagos"}
-                href4={"#"} icon4={"far fa-money-bill-alt"} card4={"Pagos en Linea"}
+                href4={"/Pagosenlinea"} icon4={"far fa-money-bill-alt"} card4={"Pagos en Linea"}
                 href5={"/"} icon5={"fas fa-sign-out-alt"} card5={"Cerrar Sesión"}
                 />
             {/* <!-- End of Sidebar --> */}
